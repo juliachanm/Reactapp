bfea75eda5b5987854b8c483acd64a8ce6aada23
juliachanm <juliamch0887@gmail.com>
2024-03-09 00:00:42 -0600

    first commit

---
+++ my-app/src/Components/PrimerComponente.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const PrimerComponente = () => { 
+    return (
+        < div>
+            <h2>Primer componente</h2>
+            <p>Este es mi app de venta en linea </p>
+        </div>
+    )
+}
